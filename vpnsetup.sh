c41db64197d56eb386e561690f584413917c0040
Ahmad Nemati <nematiprog@gmail.com>
2018-01-21 18:31:15 +0330

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='ipsec'
+YOUR_USERNAME='nemati'
+YOUR_PASSWORD='nemati'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
